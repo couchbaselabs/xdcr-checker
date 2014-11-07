8db9d6bac9bf94a530a4d805bb714721b0f66259
David Haikney <david.haikney@couchbase.com>
2014-11-07 13:35:34 +0000

    Comments and whitespace

--- src/main/java/MutationChecker.java
+++ src/main/java/MutationChecker.java
@@ -1,23 +1,36 @@
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
 import net.spy.memcached.internal.OperationCompletionListener;
 import net.spy.memcached.internal.OperationFuture;
-import net.spy.memcached.ops.StatusCode;
 
-// class used to record mutations to be checked
-class Mutation{
-	private long creationTime;
-	private String key;
-	private long expectedCAS;
-	private int retryCount;
 
+
+/*
+ * Mutation Checker
+ * ----------------
+ * Check that results from the destination cluster match the expected value 
+ * we received from the source cluster. 
+ * We have a simple class that represents each mutation and a listener which 
+ * does the actual comparison.
+ */
+
+class Mutation
+{
+	// Object that records
+	private long creationTime;  // When this update was first received
+	private String key;         // The document's key 
+	private long expectedCAS;   // Our "version-match" check
+	private int retryCount;     // Number of attempts we've made
+
+	
+	// Constructor takes a key and a CAS
 	public Mutation (String k, long CAS){
-		key = k;
-		expectedCAS = CAS;
+		key          = k;
+		expectedCAS  = CAS;
 		creationTime = System.currentTimeMillis();
+		retryCount   = 0;
 	}
 
+	// Accessors
 	public String getKey(){return key;}
 	public long getCreationTime(){return creationTime;}
 	public long getExpectedCAS(){return expectedCAS;}
@@ -26,6 +39,20 @@ class Mutation{
 }
 
 
+
+/*
+ * Listener class responsible for checking the results.
+ * Results check takes into account the following
+ * (1) Did the operation complete successfully ?
+ * (2) Did we match this version in the target cluster (CAS value) ?
+ * (3) If the comparison fails, was this for a legitimate reason? (see below)
+ * (4) Has this operation exceeded its number of lives?
+ * Legitimate reasons for failing can include:
+ * (i)  The document was updated again more recently
+ * (ii) The document on the source cluster was deleted
+ * On the final retry, before registering a failure, we go back to the source cluster
+ * to verify the version we are looking for remains valid.
+ */
 class MutationChecker implements OperationCompletionListener{
 
 	private Mutation mutation;
@@ -35,87 +62,116 @@ class MutationChecker implements OperationCompletionListener{
 		mutation = m;
 	}
 
-	// When we have exhausted all retries, check other possible legitimate 
-	// reasons for missing the mutation
-	public void lastChanceSaloon(){
-		OperationFuture<Map<String, String>> res = XDCRChecker.source_client.getKeyStats(mutation.getKey());
+	
+	// Function to perform appropriate logic if we have not been able to successfully
+	// detect a source mutation at the destination cluster
+	public void handleXDCRDiscrepancy()
+	{
+		System.out.println( "** Possible XDCR Discrepancy on KEY: " + mutation.getKey() + "\tCAS: " + mutation.getExpectedCAS());
+	}
+	
+	
+	private boolean keyChangedAtSource()
+	{
+		OperationFuture<Map<String, String>> res = XDCRChecker.sourceClient.getKeyStats(mutation.getKey());
 		Map<String, String> myMap = null;
 		try {
 			myMap = res.get();
 		} catch (Exception e){
-			System.out.println( "Exception getting last chance future!!:");
+			System.out.println( "Exception getting final retry future");
 		}
 		if (res.getStatus().isSuccess())
 		{
 			String dest_CAS = myMap.get("key_cas");
 			long compare_CAS = Long.parseLong(dest_CAS);
-			if (compare_CAS != mutation.getExpectedCAS())
-			{
-				System.out.println( "Royal Pardon 1: " + mutation.getKey() + "\tCAS differed at source"); 
-			}
-			else{
-				System.out.println( "!!!!! XDCR Failure !!!!!: " + mutation.getKey() + "\tCAS: " + mutation.getExpectedCAS());
-			}
+			return (compare_CAS != mutation.getExpectedCAS());
 		}
-		else {
-			System.out.println( "Royal Pardon 2: " + mutation.getKey() + "\treceived " + res.getStatus().getStatusCode()); 
+		else
+		{
+			System.out.println( "Couldn't validate key: " + mutation.getKey() + " from source. Received: " + res.getStatus().getStatusCode()); 
+			return true;
 		}
-		
 	}
 	
 	
-	public void rescheduleOrFail(){
-		// 
-		if (mutation.getRetryCount() < XDCRChecker.MAX_RETRIES) {
-
+	// Allow a certain number of retries before taking further action
+	// We must check for legitimate failure reasons and if none
+	// are found, declare a potential XDCR discrepancy
+	// reasons for missing the mutation
+	public void rescheduleOrComplete()
+	{
+		if (mutation.getRetryCount() < XDCRChecker.MAX_RETRIES) 
+		{
 			this.mutation.incRetryCount();
+			System.out.println(" - issuing retry number " + this.mutation.getRetryCount());
 			JobDispatcher.add(this.mutation);
 		} 
-		else { // last chance saloon
-	
-			lastChanceSaloon();
+		else if (keyChangedAtSource())	
+		{
+			// A Royal Pardon - the CAS now differs at source.
+			System.out.println(mutation.getKey() + "\tCAS differed at source"); 
+		}
+		else
+		{ 
+			this.handleXDCRDiscrepancy();
 		}
 	}
 
-	public void onComplete(OperationFuture<?> future) {
+	
+	// Function called when operation to get destination's CAS 
+	// value has completed
+	public void onComplete(OperationFuture<?> future) 
+	{
 		Object res = null;
+		// First check operation completed successfully
 		if (future.getStatus().isSuccess()) 
 		{
 
 			try {
 				res = future.get();
 			} catch (Exception e) {
-				System.out.println( "Exception getting future!!:" + mutation.getKey() + "\tCAS: " + mutation.getExpectedCAS() + "\tNum retries: " + mutation.getRetryCount()); 
+				System.out.println( "Exception getting future!:" + mutation.getKey() + "\tCAS: " + mutation.getExpectedCAS() + "\tNum retries: " + mutation.getRetryCount()); 
 			}
-			if (res != null){
+			if (res != null)
+			{
+				// Operation completed successfully - check if we got the CAS we expected
 				Map<String, String> myMap = (Map<String, String>)res;
 				String dest_CAS = myMap.get("key_cas");
 				long compare_CAS = Long.parseLong(dest_CAS);
 				if (compare_CAS != mutation.getExpectedCAS())
 				{
-					System.out.println ("CAS Mismatch for Key: " + mutation.getKey() + "Expected: " + mutation.getExpectedCAS() + " Got:" + compare_CAS + "...rescheduling ");
-					this.rescheduleOrFail();
+					System.out.print ("CAS Mismatch for Key: " + mutation.getKey() + "Source: " + mutation.getExpectedCAS() + " Dest:" + compare_CAS);
+					this.rescheduleOrComplete();
 				}
 				else
 				{
 					System.out.println ("Matched Key: " + mutation.getKey()+ " with CAS: " + mutation.getExpectedCAS());
 				} 
 			}
-			else
+			else // (res == null)
 			{
+				// Error-case. Typically don't expect to arrive here.
 				System.out.println( "Future returned null for key:" + mutation.getKey() + "\tCAS: " + mutation.getExpectedCAS() + "\tNum retries: " + mutation.getRetryCount() ); 
 			}
 		}
-		else
+		else // operation to get the CAS was unsuccessful - check why.
 		{
-			if (future.getStatus().getStatusCode() == StatusCode.ERR_NOT_FOUND){
-				System.out.println ("Key not found: " + mutation.getKey() + "...rescheduling");
-				this.rescheduleOrFail();
-
+			switch(future.getStatus().getStatusCode())
+			{
+				case ERR_NOT_FOUND:
+					System.out.print("Key: " + mutation.getKey() + " Not Found");
+					break;
+				case CANCELLED:
+					System.out.print ("Key: " + mutation.getKey() + " Op Cancelled");
+					break;
+				case TIMEDOUT:
+					System.out.print("Key: " + mutation.getKey() + " Timed Out");
+					break;
+				default:
+					System.out.println( "Unsuccessful future (" + future.getStatus().getStatusCode()+  ")for key:" + mutation.getKey() + "\tCAS: " + mutation.getExpectedCAS() + "\tNum retries: " + mutation.getRetryCount() ); 
+					break;		
 			}
-			else {
-				System.out.println( "Unsuccessful future (" + future.getStatus().getStatusCode()+  ")for key:" + mutation.getKey() + "\tCAS: " + mutation.getExpectedCAS() + "\tNum retries: " + mutation.getRetryCount() ); 
-			}
+			this.rescheduleOrComplete();
 		}
 	}
 }
--- src/main/java/XDCRChecker.java
+++ src/main/java/XDCRChecker.java
@@ -1,4 +1,5 @@
 import com.couchbase.client.CouchbaseClient;
+import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
 import com.couchbase.client.TapClient;
 
 import java.net.URI;
@@ -6,73 +7,101 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import net.spy.memcached.FailureMode;
 import net.spy.memcached.tapmessage.ResponseMessage;
 
 
+/*
+ * XDCR Checker
+ * -------------
+ *	Application to help verify a uni-directional XDCR relationship between 2 clusters.
+ *	Performs a check that each mutation on the source cluster makes it successfully to the destination cluster.
+ *  	Works by opening a TAP stream on the source cluster to receive all of the mutations and checks:
+ * 	(1) The key is available on the destination AND
+ *	(2) The key on the destination has the same CAS value
+ *
+ * 	This class provides the entry point (main) function, and creates the couchbase client connection to both clusters.
+ */
+
 public class XDCRChecker 
 {
+	// Some constants to help tune the application behaviour
+	public static final int MAX_RETRIES = 5;       // Number of attempts before failing a given key
+	public static final int RETRY_DELAY = 5000;    // Number of milliseconds in between retries
 	
-	public static final int MAX_RETRIES = 5;
-	public static final int RETRY_DELAY = 5000;
-	public static TapClient tapClient;
-	public static CouchbaseClient source_client;
-	public static CouchbaseClient dest_client;
+	// Source cluster details
+	private static final String source_bucket = "charlie";
+    private static final String source_password = "";
+    private static final String source_host_1 = "http://192.168.21.101:8091/pools";
+    private static final String source_host_2 = "http://192.168.21.101:8091/pools";
+    
+	// Destination cluster details
+	private static final String dest_bucket   = "charlie_backup";
+    private static final String dest_password   = "";
+    private static final String dest_host_1 = "http://192.168.21.103:8091/pools";
+    private static final String dest_host_2 = "http://192.168.21.104:8091/pools";
+ 
+	// Three client objects
+	public static TapClient tapClient;				// Source TAP to receive incoming mutations
+	public static CouchbaseClient sourceClient;	// Source client to validate CAS
+	public static CouchbaseClient destClient;		// Destination client to validate CAS
 	
 	public void run() throws Exception
 	{
-		  // (Subset) of nodes in the cluster to establish a connection
-        List<URI> source_hosts = Arrays.asList(   new URI("http://192.168.21.101:8091/pools"), new URI("http://192.168.21.102:8091/pools")   );
-        List<URI> dest_hosts= Arrays.asList(   new URI("http://192.168.21.103:8091/pools")  , new URI("http://192.168.21.104:8091/pools")  );
+		// Create the host lists ready for the client constructors
+		List<URI> source_hosts = Arrays.asList(new URI(source_host_1), new URI(source_host_2));
+		List<URI> dest_hosts   = Arrays.asList(new URI(dest_host_1), new URI(dest_host_2));
 
-        // Name of the Bucket to connect to
-        String source_bucket = "charlie";
-        String dest_bucket   = "charlie_backup";
-          
-        // Password of the bucket (empty) string if none
-        String password = "";
-       
-        // Connect to the Cluster
-         tapClient = new TapClient(source_hosts, source_bucket, password);
-         source_client = new CouchbaseClient(source_hosts, source_bucket, password);
-         dest_client = new CouchbaseClient(dest_hosts, dest_bucket, password);	
-    	tapClient.tapBackfill("jimmy18", 0,0, TimeUnit.MILLISECONDS);
-        	
-    	//TapStream testing =	tapClient.tapDump("zz");
-
-    	JobDispatcher dispatcher = new JobDispatcher(dest_client);
+		// Setup 
+        CouchbaseConnectionFactoryBuilder cfb = new CouchbaseConnectionFactoryBuilder();
+        cfb.setFailureMode(FailureMode.Cancel);
+        // The following parameters are included for further tuning where necessary
+        // cfb.setOpTimeout(2500);  // milliseconds for an operation to succeed
+		// cfb.setOpQueueMaxBlockTime(500); // milliseconds to allow to enqueue an operation
+		// cfb.setMaxReconnectDelay(500);
+		// cfb.setTimeoutExceptionThreshold(10);
+		// cfb.setUseNagleAlgorithm(true);
+	        
+        // Connect to the Clusters
+        tapClient = new TapClient(source_hosts, source_bucket, source_password);
+        sourceClient = new CouchbaseClient(source_hosts, source_bucket, source_password);
+        destClient = new CouchbaseClient(cfb.buildCouchbaseConnection(dest_hosts, dest_bucket, dest_password));	
+    	
+        
+        // Set up a TAP stream to retrieve ALL documents AND future updates
+        tapClient.tapBackfill("TAP:" + System.currentTimeMillis(), 0,0, TimeUnit.MILLISECONDS);
+        
+        // Invoke the thread to dispatch a check for each mutation
+        JobDispatcher dispatcher = new JobDispatcher(destClient);
     	dispatcher.start();
     	
+    	
+    	// Loop (forever) receiving mutations. Each mutation gets added to the job queue.
         while (tapClient.hasMoreMessages())
         { 
         	ResponseMessage responseMsg = tapClient.getNextMessage();   
         	if (responseMsg != null) 
         	{ 
+        		// DEBUG
 	        	//System.out.println( "key:" + responseMsg.getKey() + "\nCAS:" + responseMsg.getCas() + "\nvbucket:" + responseMsg.getVbucket() ); 
-	        	
-	        	
+      	        	
 	        	Mutation mutation = new Mutation(responseMsg.getKey(), responseMsg.getCas());
 	        	JobDispatcher.add(mutation);
-
         	}
-
-        	
-        	
         }
-        
+      
         // Shutting down properly
         tapClient.shutdown();
-
-    	
+        sourceClient.shutdown();
+        destClient.shutdown();
     }	//run
 	
 	
-	
+	// Simple main function to kick things off.
     public static void main( String[] args ) throws Exception
     {
-
-    	  	XDCRChecker myApp = new XDCRChecker();
-    	  	myApp.run();
-    	
+	  	XDCRChecker myApp = new XDCRChecker();
+	  	myApp.run();
     } //main
 
 } //class
